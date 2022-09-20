b43c432a08b50a1907d7b91d9c58fe304587c95d
morkovka951 <solar2223@gmail.com>
2022-09-20 20:17:35 +0200

    start create func work with excel file

--- getDataOneExcel.py
+++ getDataOneExcel.py
@@ -13,4 +13,7 @@ def main():
 
 def readExcel():
     wb = load_workbook('c:/Users/User/Desktop/testdata.xlsx')
-    print(wb.get_sheet_names())
+    print(wb.get_sheet_names())     # get all list in excel
+    print(wb.active)       # what list is active in excel
+    sheet = wb.get_sheet_by_name("Лист1")       #
+    print(sheet['C11'].value)
